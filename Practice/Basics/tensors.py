2308fa5babfd5ab80c2728999e7c92f21433d73c
knight <sangeetsatheesh@gmail.com>
2024-06-13 00:33:08 -0400

    Basics of tensors

---
+++ Practice/Basics/tensors.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+import numpy as np
+
+scalar = tf.constant(5)
+print(f"Scalar value:", scalar)
+print(f"Scalar via numpy:", scalar.numpy())
+print(f"Scalar dimension:", scalar.ndim)
+
+vector = tf.constant([1, 2, 3])
+print(f"Vector value:", vector)
+print(f"Vector via numpy:", vector.numpy())
+print(f"Vector dimension:", vector.ndim)
+print(f"Vector dtype:", vector.dtype)
+print(f"Vector shape:", vector.shape)
+
+matrix = tf.constant([[1, 2], [3, 4]])
+print(f"Matrix value:", matrix)
+print(f"Matrix via numpy:", matrix.numpy())
+print(f"Matrix dimension:", matrix.ndim)
+print(f"Matrix dtype:", matrix.dtype)
+print(f"Matrix shape:", matrix.shape)
+
+tensor = tf.constant(
+    [[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]], [[13, 14, 15], [16, 17, 18], [19, 20, 21], [22, 23, 24]]])
+print(f"Tensor value:", tensor)
+print(f"Tensor via numpy:", tensor.numpy())
+print(f"Tensor dimension:", tensor.ndim)
+print(f"Tensor dtype:", tensor.dtype)
+print(f"Tensor shape:", tensor.shape)
+
+changeable_tensor = tf.Variable([10., 7.])
+print(f"Changeable tensor value:", changeable_tensor)
+print(f"Changeable tensor via numpy:", changeable_tensor.numpy())
+print(f"Changeable tensor dtype:", changeable_tensor.dtype)
+print(f"Changeable tensor shape:", changeable_tensor.shape)
+changeable_tensor.assign([1, 2])
+print(f"Changeable tensor via numpy after assigning int values:", changeable_tensor.numpy())
+print(f"Changeable tensor dtype after assigning int values:", changeable_tensor.dtype)
+
+changeable_tensor2 = tf.Variable([1, 2])
+print(f"Another Changeable tensor value:", changeable_tensor2)
+print(f"Another Changeable tensor via numpy:", changeable_tensor2.numpy())
+print(f"Another Changeable tensor dtype:", changeable_tensor2.dtype)
+print(f"Another Changeable tensor shape:", changeable_tensor2.shape)
+# changeable_tensor2.assign([3., 5.])
+# print(f"Another Changeable tensor via numpy after assigning float values:", changeable_tensor2.numpy())
+# print(f"Another Changeable tensor dtype after assigning float values:", changeable_tensor2.dtype)
+# print(f"Tensor type does not change with assignment and hence cannot convert float to int and  throws error!")
+
+
+random_tensor = tf.random.Generator.from_seed(42)
+print(f"Random tensor generator with seed 42\n", random_tensor)
+random1 = random_tensor.normal(shape=(3, 2))
+print(f"Random tensor value from normal distribution:\n", random1)
+print(f"Random tensor via numpy from normal distribution:\n", random1.numpy())
+print(f"Random tensor dtype from normal distribution:", random1.dtype)
+print(f"Random tensor shape from normal distribution:", random1.shape)
+
+random2 = random_tensor.uniform(shape=(3, 2))
+print(f"Random tensor value from uniform distribution:\n", random2)
+print(f"Random tensor via numpy from uniform distribution:\n", random2.numpy())
+print(f"Random tensor dtype from uniform distribution:", random2.dtype)
+print(f"Random tensor shape from uniform distribution:", random2.shape)
+
+# Shuffle
+
+not_shuffled = tf.constant([[1, 2], [3, 4], [5, 6]])
+print(f"Not shuffled tensor values:\n", not_shuffled.numpy())
+tf.random.set_seed(42)
+# print(f"After shuffling tensor :\n", tf.random.shuffle(not_shuffled))
+not_shuffled = tf.random.shuffle(not_shuffled)
+print(f"After shuffling tensor :\n", not_shuffled.numpy())
