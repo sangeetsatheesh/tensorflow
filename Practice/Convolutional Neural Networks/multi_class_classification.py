50ce3819f4d6e924513dc6b3086757dcfc53b6ad
sangeetsatheesh <sangeetsatheesh@gmail.com>
2024-06-27 02:06:47 -0400

    Sign Language MNIST

---
+++ Practice/Convolutional Neural Networks/multi_class_classification.py
@@ -0,0 +1,10 @@
+"""
+Sign Language MNIST
+"""
+import csv
+import string
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img
+
