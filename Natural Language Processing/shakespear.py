26d1c68b5b5e732daa9d3a9875fd0b991ede87f9
sangeetsatheesh <sangeetsatheesh@gmail.com>
2024-06-29 01:57:24 -0400

    accuracy: 0.8523 - loss: 0.5102
    Help me Obi Wan Kenobi, you're my only hope well telling my heart doth face have seen thy song shall thee doth live bright live blind thine or be thy defect receivest in me for truth doth near face sang crave of trust you doth latch latch latch thee bright now you be old same it thought new not so great verse can crave near crave gone blind and bright in trust for mine eye aside aside a part of new pride report new so self love held live young not beauty's painted bright than his chest lie hid to thee bright hold say live blind last so dumb

--- Natural Language Processing/shakespear.py
+++ Natural Language Processing/shakespear.py
@@ -6,4 +6,205 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
+import gdown
 
+id = "108jAePKK4R3BVYBbYJZ32JWUwxeMg20K"
+
+gdown.download(id=id)
+
+SONNETS_FILE = "./sonnets.txt"
+
+with open(SONNETS_FILE) as f:
+    data = f.read()
+
+corpus = data.lower().split("\n")
+print(f"Corpus is \n{corpus}")
+print(f"There are {len(corpus)} lines of sonnets")
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(corpus)
+total_words = len(tokenizer.word_index) + 1
+
+print(f"Corpus 0th element: ", corpus[0])
+
+print(tokenizer.texts_to_sequences(corpus[0]))
+
+print(tokenizer.texts_to_sequences([corpus[0]]))  # Must place the example within a list
+print(tokenizer.texts_to_sequences([corpus[0]])[0])  # To get the first item in the list
+
+
+# Generate n_grams
+
+def n_gram_seqs(corpus, tokenizer):
+    """
+    Generates a sequence of n-gram sequences
+    :param corpus:
+    :param tokenizer:
+    :return:
+    """
+    input_sequences = []
+    for line in corpus:
+        token_list = tokenizer.texts_to_sequences([line])[0]
+        for i in range(1, len(token_list)):
+            n_gram_sequence = token_list[:i + 1]
+            input_sequences.append(n_gram_sequence)
+    return input_sequences
+
+
+# Test your function with one example
+first_example_sequence = n_gram_seqs([corpus[0]], tokenizer)
+
+print("n_gram sequences for first example look like this:\n")
+print(first_example_sequence)
+# Test your function with a bigger corpus
+next_3_examples_sequence = n_gram_seqs(corpus[1:4], tokenizer)
+
+print("n_gram sequences for next 3 examples look like this:\n")
+print(next_3_examples_sequence)
+
+# Apply the n_gram_seqs transformation to the whole corpus
+input_sequences = n_gram_seqs(corpus, tokenizer)
+
+# Save max length
+max_sequence_len = max([len(x) for x in input_sequences])
+
+print(f"n_grams of input_sequences have length: {len(input_sequences)}")
+print(f"maximum length of sequences is: {max_sequence_len}")
+
+
+def pad_seqs(input_sequences, maxlen):
+    """
+    Pads tokenized sequences to the same length
+
+    Args:
+        input_sequences (list of int): tokenized sequences to pad
+        maxlen (int): maximum length of the token sequences
+
+    Returns:
+        padded_sequences (array of int): tokenized sequences padded to the same length
+    """
+    ### START CODE HERE
+    padded_sequences = np.array(pad_sequences(input_sequences, maxlen=maxlen, padding='pre'))
+
+    return padded_sequences
+
+
+first_padded_seq = pad_seqs(first_example_sequence, max([len(x) for x in first_example_sequence]))
+print(first_padded_seq)
+
+next_3_padded_seq = pad_seqs(next_3_examples_sequence, max([len(s) for s in next_3_examples_sequence]))
+print(next_3_padded_seq)
+
+input_sequences = pad_seqs(input_sequences, max_sequence_len)
+
+print(f"padded corpus has shape: {input_sequences.shape}")
+
+
+def features_and_labels(input_sequences, total_words):
+    """
+    Generates features and labels from n-grams
+
+    Args:
+        input_sequences (list of int): sequences to split features and labels from
+        total_words (int): vocabulary size
+
+    Returns:
+        features, one_hot_labels (array of int, array of int): arrays of features and one-hot encoded labels
+    """
+    ### START CODE HERE
+    features = input_sequences[:, :-1]
+    labels = input_sequences[:, -1]
+    one_hot_labels = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+    ### END CODE HERE
+
+    return features, one_hot_labels
+
+
+first_features, first_labels = features_and_labels(first_padded_seq, total_words)
+
+print(f"labels have shape: {first_labels.shape}")
+print("\nfeatures look like this:\n")
+print(first_features)
+
+features, labels = features_and_labels(input_sequences, total_words)
+
+print(f"features have shape: {features.shape}")
+print(f"labels have shape: {labels.shape}")
+
+
+def create_model(total_words, max_sequence_len):
+    """
+    Creates a text generator model
+
+    Args:
+        total_words (int): size of the vocabulary for the Embedding layer input
+        max_sequence_len (int): length of the input sequences
+
+    Returns:
+        model (tf.keras Model): the text generator model
+    """
+    model = Sequential()
+    model.add(Embedding(total_words, 100, input_length=max_sequence_len - 1))  # Your Embedding Layer
+    model.add(Bidirectional(LSTM(150, return_sequences=True)))  # An LSTM Layer
+    model.add(tf.keras.layers.Dropout(0.2))  # A dropout layer
+    model.add(LSTM(100))  # Another LSTM Layer
+    model.add(Dense(total_words * 2, activation='relu'))  # A Dense Layer including regularizers
+    model.add(Dense(total_words, activation='softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
+
+
+model = create_model(total_words, max_sequence_len)
+
+# Train the model
+history = model.fit(features, labels, epochs=50, verbose=1)
+
+acc = history.history['accuracy']
+loss = history.history['loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'b', label='Training accuracy')
+plt.title('Training accuracy')
+
+plt.figure()
+
+plt.plot(epochs, loss, 'b', label='Training Loss')
+plt.title('Training loss')
+plt.legend()
+
+plt.show()
+
+
+def download_history():
+    import pickle
+
+    with open('history.pkl', 'wb') as f:
+        pickle.dump(history.history, f)
+
+
+download_history()
+
+seed_text = "Help me Obi Wan Kenobi, you're my only hope"
+next_words = 100
+
+for _ in range(next_words):
+    # Convert the text into sequences
+    token_list = tokenizer.texts_to_sequences([seed_text])[0]
+    # Pad the sequences
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+    # Get the probabilities of predicting a word
+    predicted = model.predict(token_list, verbose=0)
+    # Choose the next word based on the maximum probability
+    predicted = np.argmax(predicted, axis=-1).item()
+    # Get the actual word from the word index
+    output_word = tokenizer.index_word[predicted]
+    # Append to the current text
+    seed_text += " " + output_word
+
+print(seed_text)
+
+model.save('shakespear.keras')
