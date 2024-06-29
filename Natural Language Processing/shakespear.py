0a3de70541c479deac991ed1105ea9057b068370
sangeetsatheesh <sangeetsatheesh@gmail.com>
2024-06-29 00:38:16 -0400

    Shakespear

--- Natural Language Processing/shakespear.py
+++ Natural Language Processing/shakespear.py
@@ -1,11 +1,9 @@
 import tensorflow as tf
-
 import numpy as np
-import os
-import time
-path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
-# Read, then decode for py2 compat.
-text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
-# length of text is the number of characters in it
-print(f'Length of text: {len(text)} characters')
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
 
